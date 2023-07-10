300206b6963bfe1b04a051c025872212e897c05f
amit dubey <amitdubey6261@gmail.com>
2023-07-10 22:06:48 +0530

    socket

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ import path from 'path';
 import cors from 'cors'; 
 
 //my imports 
-import Hello from './SocketModule/Hello.js' ; 
 
 
 class SocketServer{
     constructor(){
         this.setunCaughtException();
         this.setMiddleWares();
-
-        this.hello = new Hello() ; 
         this.createSocketServer() ; 
     }
 
@@ -34,22 +31,63 @@ class SocketServer{
         const app = express() ;
         app.use(express.json()); 
         app.use(cors());
-
+        
         this.httpServer = createServer(app) ;
         this.io = new Server( this.httpServer , { cors : {} , maxHttpBufferSize : 1e8 }) ; 
 
+        this.roomData = {} ; 
+
         this.io.on('connection' , (socket)=>{
-            console.log(`Connection Successfull ` , socket.id ) ; 
-            socket.on('HELLO' , this.hello.boloHello ) ; 
+            console.log(`Connected SocketID:${socket.id}`);
+
+            socket.on('create_room' , (roomId)=>{
+                socket.join(roomId);
+                socket.emit('created_room' , roomId ); 
+            })
+
+            socket.on( 'join_room' , (roomId)=>{ 
+                if( ! this.io.sockets.adapter.rooms.has(roomId) ){
+                    socket.emit('Error' , "Not Found Room ") ; 
+                }
+                else{
+                    if( this.io.sockets.adapter.rooms.get(roomId).size > 3 ){
+                        console.log('ROOOM LIMIT REACH') ; 
+                        socket.emit('Error' , 'room in maximum limit'); 
+                    }
+                    else{
+                        socket.join(roomId);
+                        console.log('andar' , socket.id , this.io.sockets.adapter.rooms.get(roomId).size ) ; 
+                        if( this.io.sockets.adapter.rooms.get(roomId).size === 2 ){
+                            console.log(`4 users joined in a ${roomId}`) ; 
+                            console.log('start');
+
+                            this.roomData[roomId] = this.getDefaultPosition() ; 
+
+                            console.log(this.roomData[roomId]) ; 
+
+
+
+
+                        }
+                    }
+                }
+            })
         })
 
         this.httpServer.listen(process.env.PORT , ()=>{
-            `App started listening on port ${process.env.PORT}`
+            console.log(`App started listening on port ${process.env.PORT}`) ; 
         })
+    }
 
-        app.get('*' , (req , res)=>{
-            res.send('Hello');
-        })
+    getDefaultPosition(){
+        const tokens = new Array();
+        const defaultTokenPosition = [ [3,3] , [3,12] , [11,12] , [11,3] ] ; 
+        for (let i = 0; i < 4; i++) {
+            const tokenArray = new Array(4).fill({ position: { x: defaultTokenPosition[i][0], y: defaultTokenPosition[i][1], z: 1 } });
+            tokens.push(tokenArray);
+        }
+
+        return tokens ; 
     }
 }
 
